6fd78794755e6f31fef042e5c31f1b48faed32ed
Rafael <rafael_oliveira@gmx.com>
2021-03-20 18:36:14 -0300

    Updated header styles

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,12 @@
 import headerStyles from "../styles/Header.module.css"; 
 
 const Header = () => {
+  const color = Math.random();
+
   return (
     <div>
-      <h1 className={headerStyles.title}> <span>Dev</span> News</h1>
+      {/* You can use use dynamic stylling with the css module, since it is just returning a string class name */}
+      <h1 className={headerStyles.title + " " + (color <= 0.5 ? headerStyles.title2 : null)}> <span>Dev</span> News</h1>
       <p className={headerStyles.description}>Keep up to date with the latest dev news!</p>
     </div>
   )

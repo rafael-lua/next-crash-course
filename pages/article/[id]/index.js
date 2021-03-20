87459ee07155c958a5c8af28ddd9e711a3b3871b
Rafael <rafael_oliveira@gmx.com>
2021-03-20 20:34:46 -0300

    Updated fetch to serverSide

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,14 +1,59 @@
-import { useRouter } from "next/router";
+import Link from "next/link"; 
+// import { useRouter } from "next/router";
 
-const article = () => {
-  const router = useRouter();
-  const {id} = router.query;
+const article = (props) => {
+  /* Example of data being used with "getStaticProps" from index.  */
+  // const router = useRouter();
+  // const {id} = router.query;
 
   return (
-    <div>
-      this is article {id}
-    </div>
+    <>
+      <h1>{props.article.title}</h1>
+      <p>{props.article.body}</p>
+      <br/>
+      <Link href="/">Go Back</Link>
+    </>
   )
 }
+
+// Fetch data on each request with server side props
+export const getServerSideProps = async (context) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+  const article = await res.json();
+
+  return {
+    props: {
+      article
+    }
+  }
+}
+
+/* EXTRA: Using staticProps and paths generation */
+
+// export const getStaticProps = async (context) => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+//   const article = await res.json();
+
+//   return {
+//     props: {
+//       article
+//     }
+//   }
+// }
+
+// // Generates the data for dynamic paths. Use it together with getStaticProps. Good for static sites
+// export const getStaticPaths = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
+//   const articles = await res.json();
+
+//   const ids = articles.map((article) => article.id);
+//   const paths = ids.map((id) => ({params: {id: id.toString()}}));
+
+//   return {
+//     // paths: { params: { id: "1", id: "2" }} // The paths model. You need to create an object params like that from the json data
+//     paths,
+//     fallback: false
+//   }
+// }
 
 export default article
--- pages/index.js
+++ pages/index.js
@@ -15,11 +15,12 @@ export default function Home(props) {
   )
 }
 
-// fetch props
+// fetch props at build time
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=6");
   const articles = await res.json();
 
+  // will be passed to the page component as props
   return {
     props: {
       articles
